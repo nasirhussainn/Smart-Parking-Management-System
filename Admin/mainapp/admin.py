479b1fede4eb8455eff99c5a477a06e936ea7be5
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2025-01-22 17:52:26 +0500

    Delete Admin directory

--- Admin/mainapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import*
-
-admin.site.register(CustomUser)
-admin.site.register(ParkingSpace)
-admin.site.register(Slot_reservation)
-
